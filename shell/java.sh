5591be544f474c8fa5d8bc201eb14c16ef249aa2
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2015-01-07 23:49:35 -0200

    Java com tar.gz

--- shell/java.sh
+++ shell/java.sh
@@ -1,6 +1,23 @@
-wget --no-cookies \
---no-check-certificate \
---header "Cookie: oraclelicense=accept-securebackup-cookie" \
-"http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-linux-x64.rpm" \
--O jdk-8-linux-x64.rpm
-rpm -i jdk-8-linux-x64.rpm
+if [ ! -f jdk.tar.gz ]; then
+  wget --no-cookies \
+  --no-check-certificate \
+  --header "Cookie: oraclelicense=accept-securebackup-cookie" \
+  "http://download.oracle.com/otn-pub/java/jdk/8u25-b17/jdk-8u25-linux-x64.tar.gz" \
+  -O jdk.tar.gz
+fi
+mkdir -p /opt/jdk
+tar -xvzf jdk.tar.gz -C /opt/jdk --strip-components=1
+touch /etc/profile.d/jdk.sh
+cat > /etc/profile.d/jdk.sh << EOL
+#!/bin/bash
+JAVA_HOME=/opt/jdk
+PATH=\$JAVA_HOME/bin:\$PATH && export PATH
+export JAVA_HOME
+CLASSPATH=\$JAVA_HOME/lib/tools.jar
+CLASSPATH=.:\$CLASSPATH
+export CLASSPATH
+EOL
+chmod +x /etc/profile.d/jdk.sh
+source /etc/profile.d/jdk.sh
+java -version
+javac -version
--- shell/maven.sh
+++ shell/maven.sh
@@ -1,5 +1,7 @@
-wget http://www.eng.lsu.edu/mirrors/apache/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz \
--O apache-maven.tar.gz
+if [ ! -f apache-maven.tar.gz ]; then
+  wget http://www.eng.lsu.edu/mirrors/apache/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz \
+  -O apache-maven.tar.gz
+fi
 mkdir -p /opt/maven
 tar -xvzf apache-maven.tar.gz -C /opt/maven --strip-components=1
 ln -s /opt/maven/bin/mvn /usr/bin/mvn
@@ -7,12 +9,10 @@ touch /etc/profile.d/maven.sh
 cat > /etc/profile.d/maven.sh << EOL
 #!/bin/bash
 MAVEN_HOME=/opt/maven
-PATH=$MAVEN_HOME/bin:$PATH
+PATH=\$MAVEN_HOME/bin:\$PATH
 export PATH MAVEN_HOME
 export CLASSPATH=.
 EOL
 chmod +x /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
 mvn -version
-echo $MAVEN_HOME
-echo $PATH
